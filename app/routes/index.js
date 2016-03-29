8f0d35a79637bff2c2eb407f5845a3c1be562251
Blake Scott <blakerscott@gmail.com>
2016-03-29 15:50:09 -0700

    add ability to display articles from blake's firebase database

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,29 +1,7 @@
 import Ember from 'ember';
 
-var articles = [{
-  id: 1,
-  title: "bustle is a bad site",
-  author: "anonymous",
-  content: "lorem ipsum blah",
-  image: "http://lorempixel.com/400/200/sports",
-  tags: ["entertainment", "news"]
-}, {
-  id: 2,
-  title: "Stuff is on the internet yo",
-  author: "Otto",
-  content: "lorem ipsum blah blah blah",
-  image: "http://lorempixel.com/400/200/sports",
-  tags: ["entertainment", "lifestyle"]
-}, {
-  id: 3,
-  title: "People obsessed with collecting cats",
-  author: "A yahoo answers user",
-  content: "lorem ipsum blah blah meow meow",
-  image: "http://lorempixel.com/400/200/sports",
-  tags: ["news", "books"]
-}];
 export default Ember.Route.extend({
   model() {
-    return articles;
-  }
+    return this.store.findAll('article');
+  },
 });

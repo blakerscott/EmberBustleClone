f54e517cb4e7ee20d406371b08516775bf1a6139
Blake Scott <blakerscott@gmail.com>
2016-03-29 15:17:47 -0700

    add seed.json file

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,21 +5,21 @@ var articles = [{
   title: "bustle is a bad site",
   author: "anonymous",
   content: "lorem ipsum blah",
-  image: "http://lorempixel.com/image_output/sports-q-c-640-480-9.jpg",
+  image: "http://lorempixel.com/400/200/sports",
   tags: ["entertainment", "news"]
 }, {
   id: 2,
   title: "Stuff is on the internet yo",
   author: "Otto",
   content: "lorem ipsum blah blah blah",
-  image: "http://lorempixel.com/image_output/sports-q-c-640-480-9.jpg",
+  image: "http://lorempixel.com/400/200/sports",
   tags: ["entertainment", "lifestyle"]
 }, {
   id: 3,
   title: "People obsessed with collecting cats",
   author: "A yahoo answers user",
   content: "lorem ipsum blah blah meow meow",
-  image: "http://lorempixel.com/image_output/sports-q-c-640-480-9.jpg",
+  image: "http://lorempixel.com/400/200/sports",
   tags: ["news", "books"]
 }];
 export default Ember.Route.extend({
